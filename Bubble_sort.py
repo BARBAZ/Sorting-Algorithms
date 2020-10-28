b76d23871c332e616dd3694b52a1b010b2ac03cb
barbaz <kevin_barbazan@live.fr>
2020-10-28 11:23:50 +0100

    Overall code cleaning | Added Chrono_print()

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -2,7 +2,7 @@
 # Optimisation tests
 
 # Data sets and array are defined  before chrono start 
-# to only mesure computational time
+# to mesure only computational time
 
 # Modules
 
@@ -18,22 +18,36 @@ Sorted_Array = []
 
 def fill_list(items_numbers):
     for i in range(items_numbers):
-        Dataset.append(random.randrange(0, 999999999))
+        Dataset.append(random.randrange(0, 999999999)) # picks random values included between 0 - 999999999 and append it to an array
+
+def Chrono_print(comp_cnt):
+    string = "Elapsed time :"
+    unit = ".s"
+    string2 = " Comparisons"
+    time = end - start
+    time = str(time)
+    comp_cnt = str(comp_cnt)
+    tuple0 = (string, time, unit)
+    tuple1 = (comp_cnt, string2)
+    log_time = "".join(tuple0)
+    log_comp = "".join(tuple1)
+    print log_time 
+    print log_comp
+    
 
 # Variables 
 
 loop = 1
-
-fill_list(12000)
-start = time.time() # Chrono start
+fill_list(7500) # sets the amount of randomally generated numbers <= 2500 # we recommand values < 10000 #  be careful N^2 sorting times are exponenential !
 comp_count = 0
 
-## Algo ##
+
+start = time.time() # Chrono start
 
 while(loop):
     index = -1
-    Min = 10000000000
-    Min_index = 0
+    Min = 1000000000 # Re init Var each cycle
+    Min_index = 0 # Re init Var each cycle
     for i in range(len(Dataset)):
         index += 1
         Var = Dataset[i]
@@ -48,11 +62,4 @@ while(loop):
 
 end = time.time() # Chrono stop
 
-string = "Elapsed time :"
-time = end - start
-time = str(time)
-tuple = (string, time)
-log_time = "".join(tuple)
-print log_time 
-print comp_count
-print "Comparisons"
+Chrono_print(comp_count)
